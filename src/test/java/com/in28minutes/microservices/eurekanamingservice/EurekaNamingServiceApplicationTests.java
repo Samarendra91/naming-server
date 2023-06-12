2cdcaaaac5e318406643693850ca30031cbc4df8
Samarendra Sahoo <30408568+Samarendra91@users.noreply.github.com>
2023-06-12 10:44:25 +0530

    Add files via upload

---
+++ src/test/java/com/in28minutes/microservices/eurekanamingservice/EurekaNamingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.eurekanamingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaNamingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
